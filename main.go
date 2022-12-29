79a3fb6b13ba72089c0673af5ab1a0836e9d9b73
omer ben david <omerbendavid21@gmail.com>
2022-12-29 21:09:09 +0200

    added 1 minute resync period

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ import (
 
 	"flag"
 	"os"
+	"time"
 
 	"go.elastic.co/ecszap"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -69,6 +70,7 @@ func main() {
 	log := zap.New(core, zap.AddCaller())
 	logf.SetLogger(zapr.NewLogger(log))
 
+	minute := time.Minute
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
@@ -76,6 +78,7 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "8bfebfd7.benda.io",
+		SyncPeriod:             &minute,
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
